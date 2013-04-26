94a784dd03c2d5028c268cff37848d3670661ccd
getsometoast <james.lewis@7digital.com>
2013-04-26 17:27:29 +0100

    refactor out the duplicate logic into methods

--- src/test/java/org/katas/checkout/CheckoutTest.java
+++ src/test/java/org/katas/checkout/CheckoutTest.java
@@ -5,6 +5,12 @@ import org.junit.*;
 
 public class CheckoutTest 
 {	
+	@Before
+	public void Setup()
+	{
+		_totalPrice = 0;
+	}
+	
     @Test
     public void should_return_zero_total_price_when_no_items_added_to_basket()
     {
@@ -18,30 +24,39 @@ public class CheckoutTest
     public void should_return_60_total_price_when_1_item_A_added_to_basket()
     {	
     	int expectedTotalPrice = 60;
-    	int itemAPrice = 60;
     	char itemASku = 'A';
-    	int totalPrice = 0;
 
-    	if(itemASku == 'A')
-    		totalPrice += itemAPrice;
+    	addItemToBasket(itemASku);
     	
-    	assertEquals(expectedTotalPrice, totalPrice);
+    	assertEquals(expectedTotalPrice, _totalPrice);
     }
     
     @Test
     public void should_return_120_total_price_when_2_item_A_added_to_basket()
     {
     	int expectedTotalPrice = 120;
-    	int itemAPrice = 60;
     	char itemASku = 'A';
-    	int totalPrice = 0;
     	
-    	if(itemASku == 'A')
-    		totalPrice += itemAPrice;
-
-    	if(itemASku == 'A')
-    		totalPrice += itemAPrice;
+    	addItemToBasket(itemASku);
+    	addItemToBasket(itemASku);
     	
-    	assertEquals(expectedTotalPrice, totalPrice);
+    	assertEquals(expectedTotalPrice, _totalPrice);
     }
+
+    
+    
+    private int _totalPrice = 0;
+    
+	private int getTotalPrice()
+	{
+		return _totalPrice;
+	}
+    
+	private void addItemToBasket(char itemSku) 
+	{
+		final int itemAPrice = 60;
+		
+		if(itemSku == 'A')
+    		_totalPrice += itemAPrice;
+	}
 }
