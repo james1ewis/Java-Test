c4b57ba88b34d11548df766c86176f6649fae412
getsometoast <james.lewis@7digital.com>
2013-04-26 16:28:04 +0100

    add first test for checkout kata

--- src/test/java/org/katas/checkout/CheckoutTest.java
+++ src/test/java/org/katas/checkout/CheckoutTest.java
@@ -1,13 +1,20 @@
 package org.katas.checkout;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.*;
 
 public class CheckoutTest 
 {
+	public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(CheckoutTest.class);
+    }
+	
     @Test
-    public void testApp()
+    public void should_return_zero_total_price_when_no_items_added_to_basket()
     {
-        assertTrue( true );
+    	int expectedTotalPrice = 0;
+		int totalPrice = 0;
+		
+		assertEquals(expectedTotalPrice, totalPrice);
     }
 }
