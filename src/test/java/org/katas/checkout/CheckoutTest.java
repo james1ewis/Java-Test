00ec9c8a19c2c9728aed835a6cf7b4df77d3c152
getsometoast <james.lewis@7digital.com>
2013-04-26 17:37:08 +0100

    remove unrequired @Before and test for 0 items

--- src/test/java/org/katas/checkout/CheckoutTest.java
+++ src/test/java/org/katas/checkout/CheckoutTest.java
@@ -4,22 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class CheckoutTest 
-{	
-	@Before
-	public void Setup()
-	{
-		_totalPrice = 0;
-	}
-	
-    @Test
-    public void should_return_zero_total_price_when_no_items_added_to_basket()
-    {
-    	int expectedTotalPrice = 0;
-		int totalPrice = 0;
-		
-		assertEquals(expectedTotalPrice, totalPrice);
-    }
-    
+{   
     @Test
     public void should_return_60_total_price_when_1_item_A_added_to_basket()
     {	
