342853ff0f2a1d959bc8a04eef93383fdecd760b
getsometoast <james.lewis@7digital.com>
2013-04-26 17:16:40 +0100

    add latest junit ass maven dependency

--- src/test/java/org/katas/checkout/CheckoutTest.java
+++ src/test/java/org/katas/checkout/CheckoutTest.java
@@ -4,11 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class CheckoutTest 
-{
-	public static junit.framework.Test suite() {
-        return new junit.framework.JUnit4TestAdapter(CheckoutTest.class);
-    }
-	
+{	
     @Test
     public void should_return_zero_total_price_when_no_items_added_to_basket()
     {
